f7de9775acb750cbc467ec518108c784a8f18817
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2023-02-10 17:52:36 -0500

    Channels that are percentages are now between 0-100% when acquired from get_lap_data()

--- scripts/iRTLData.py
+++ scripts/iRTLData.py
@@ -6,6 +6,7 @@ Kyle Ward 2023
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class iRTLDataProcessor:
     """
@@ -60,6 +61,10 @@ class iRTLDataProcessor:
             for err_idx in err_idxs:
                 _data[err_idx] = _data[err_idx-1]
             
+            # Check if channel unit is a percentage
+            if self.data[channel]["unit"] == "%":
+                _data *= 100
+            
             lap_data[channel] = {
                 "desc": self.data[channel]["desc"],
                 "unit": self.data[channel]["unit"],
@@ -78,3 +83,27 @@ class iRTLDataProcessor:
         # Get the lap points for the specified lap
         lap_pts = self.lap_points[lap-1]
         return self.data["time"]["data"][lap_pts[1]] - self.data["time"]["data"][lap_pts[0]]
+    
+    def plot_channel_across_lap(self, channel: str, lap: int):
+        """
+        Plot the specified channel across the specified lap
+
+        Args:
+            lap (int): Lap number
+            channel (str): Channel name
+        """
+        # Check that the channel is in the list of selected channels to record
+        if not channel in self.data.keys():
+            raise Exception(f"iRTLDataProcessor.plot_channel_across_lap(): Channel '{channel}' not found in the data!")
+        if lap > self.n_laps:
+            raise Exception(f"iRTLDataProcessor.get_lap_time(): Lap {lap} does not exist!")
+        
+        # Get lap data and extract the channel data
+        lap_data = self.get_lap_data(lap)
+        x = lap_data["time"]["data"]
+        plt.title(f"{channel} (Lap {lap})")
+        plt.xlabel("Time (s)")
+        plt.ylabel(f"{channel} ({lap_data[channel]['unit']})")
+        plt.plot(x, lap_data[channel]["data"], label=channel)
+        plt.legend()
+        plt.show()
