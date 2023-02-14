232ed0711bcaa56994594aab4098669ed891f30f
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2023-02-14 18:03:49 -0500

    Started adding build_widgets()

--- gui/main_screen.py
+++ gui/main_screen.py
@@ -17,10 +17,31 @@ class MainScreen(ctk.CTkFrame):
         
         # Create test button
         self.widgets = {
-            "buttons": {}
+            "buttons": {},
+            "labels": {},
+            "tabs": {}
         }
         
-        self.font_size = 20
+        # Default font size
+        self.font_size = 20 
         
-        self.widgets["buttons"]["test_button"] = ctk.CTkButton(self, text="Test", command=lambda: print("Test button pressed"), fg_color=COLORS["royal_purple"], font=("Arial", 20), cursor="hand2")
+        # Build widgets
+        self.build_widgets()
+        
+    def build_widgets(self):
+        """
+        Build GUI widgets and add them to the frame
+        """
+        # Create title label
+        self.widgets["labels"]["title"] = ctk.CTkLabel(self, text="iRacing Telemetry Logger", text_color=COLORS["text_white"], font=("Arial", self.font_size+15))
+        self.widgets["labels"]["title"].place(relx=0.5, rely=0.05, anchor="center")
+        
+        # Build buttons
+        self.widgets["buttons"]["test_button"] = ctk.CTkButton(self, 
+                                                               text="Test", 
+                                                               command=lambda: print("Test button pressed"), 
+                                                               fg_color=COLORS["royal_purple"],
+                                                               hover_color=COLORS["btn_hover"],
+                                                               font=("Arial", self.font_size),
+                                                               cursor="hand2")
         self.widgets["buttons"]["test_button"].place(relx=0.5, rely=0.5, anchor="center")
