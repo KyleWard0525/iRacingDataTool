e8a3e654ad8f01de729318ef4f6a0ad1b224f0f3
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2023-02-14 17:49:54 -0500

    Initialize GUI module

---
+++ gui/__init__.py
@@ -0,0 +1,14 @@
+"""
+GUI module for iRacing Telemetry Logger
+
+Copyright © Kyle Ward 2023    
+"""
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark") # Dark mode
+ctk.set_default_color_theme("dark-blue") # Dark blue theme
+
+COLORS = {
+    "royal_purple": "#570091"
+}
+
