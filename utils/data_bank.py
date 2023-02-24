3e565f2df746ffd86a2984645b5b757c0de291b2
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2023-02-24 16:55:11 -0500

    Added data variables for storing channel logging states

--- utils/data_bank.py
+++ utils/data_bank.py
@@ -4,7 +4,7 @@ Data storage class for passing data between UI screens and modules
     
 Copyright © Kyle Ward 2023
 """
-
+from logger import CHANNELS
 
 class DataBank:
     
@@ -16,4 +16,9 @@ class DataBank:
         # Initialize internal data dictionary
         self.data = {
             "is_recording": False,
+            "channels": {}
         }
+        
+        for category in CHANNELS:
+            for channel in CHANNELS[category]:
+                self.data["channels"][channel] = 0  # 0 = don't log channel data, 1 = log channel data
