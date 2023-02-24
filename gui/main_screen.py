2aadf051e34f408d4af373eb0e96cc859c2700b7
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2023-02-24 15:59:36 -0500

    Started building channel selection page

--- gui/main_screen.py
+++ gui/main_screen.py
@@ -8,6 +8,8 @@ import customtkinter as ctk
 from PIL import Image
 from gui import COLORS
 from utils.data_bank import DataBank
+from tktooltip import ToolTip
+from logger import CHANNELS
 
 class MainScreen(ctk.CTkFrame):
     
@@ -22,17 +24,23 @@ class MainScreen(ctk.CTkFrame):
         # Set data bank object
         self.data_bank = data_bank
         
-        # Create test button
+        # UI widgets
         self.widgets = {
             "buttons": {},
             "labels": {},
             "images": {},
-            "tabs": {}
+            "tabs": {},
+            "tooltips": {},
+            "inputs": {},
+            "frames": {}
         }
+        
+        self.channel_categories = [category for category in CHANNELS.keys()]
         
         # Font sizes
         self.btn_font_size = 20 
-        self.title_font_size = 35
+        self.title_font_size = 30
+        self.input_label_font_size = 15
         
         # Build widgets
         self.build_widgets()
@@ -47,13 +55,13 @@ class MainScreen(ctk.CTkFrame):
         
         
         # Create 'Recording' status image
-        self.widgets["images"]["record_status"] = ctk.CTkImage(light_image=Image.open("images/circle.png"), dark_image=Image.open("images/circle.png"), size=(40, 40))
+        self.widgets["images"]["record_status"] = ctk.CTkImage(light_image=Image.open("images/circle.png"), dark_image=Image.open("images/circle.png"), size=(35, 35))
         
         # Create 'Recording' status label
         self.widgets["labels"]["record_status"] = ctk.CTkLabel(self,
                                                                text="Not recording",
                                                                text_color=COLORS["text_white"],
-                                                               font=("Arial", self.title_font_size-5)
+                                                               font=("Arial", self.title_font_size-10)
                                                                )
         self.widgets["labels"]["record_status"].place(relx=0.125, rely=0.05, anchor="center")
         self.widgets["labels"]["record_status_image"] = ctk.CTkLabel(self, text="", image=self.widgets["images"]["record_status"])
@@ -61,13 +69,30 @@ class MainScreen(ctk.CTkFrame):
         
         
         # Create tabs
+        self.create_tabs()
+        
+        # Create tooltips
+        self.create_tooltips()
+        
+        
+    def create_tabs(self):
+        """
+        Create tabs for each screen
+        """
+        # Create tab view and place it in the frame
         self.widgets["tabs"] = ctk.CTkTabview(self.root)
-        self.widgets["tabs"].add("Home")
-        self.widgets["tabs"].add("Channels")
         self.widgets["tabs"].place(relx=0.5, rely=0.55, relwidth=1, relheight=0.9, anchor="center")
         
-        # Configure tabs
-        self.widgets["tabs"].configure(border_width=0, fg_color="#000000")
+        # Create tabs
+        self.create_home_tab()
+        self.create_channels_tab()
+    
+    def create_home_tab(self):
+        """
+        Create the home tab
+        """
+        # Add a frame to the tab view for the home tab
+        self.widgets["tabs"].add("Home")
         
         # Build buttons
         self.widgets["buttons"]["test_button"] = ctk.CTkButton(self.widgets["tabs"].tab("Home"), 
@@ -78,7 +103,99 @@ class MainScreen(ctk.CTkFrame):
                                                                font=("Arial", self.btn_font_size),
                                                                cursor="hand2")
         self.widgets["buttons"]["test_button"].place(relx=0.5, rely=0.5, anchor="center")
+    
+    def create_channels_tab(self):
+        """
+        Create the channel selection tab
+        """
+        # Add a frame to the tab view for the channel selection tab
+        self.widgets["tabs"].add("Channels")
         
+        # Add screen label
+        self.widgets["labels"]["channels_label"] = ctk.CTkLabel(self.widgets["tabs"].tab("Channels"),
+                                                                text="Channel Selection",
+                                                                text_color=COLORS["text_white"], 
+                                                                font=("Arial", self.title_font_size-2)
+                                                                )
+        self.widgets["labels"]["channels_label"].place(relx=0.5, rely=0.045, anchor="center")
+        
+        # Add label for category drop-down menu
+        self.widgets["labels"]["category_label"] = ctk.CTkLabel(self.widgets["tabs"].tab("Channels"), 
+                                                                text="Category:", 
+                                                                text_color=COLORS["text_white"], 
+                                                                font=("Arial", self.btn_font_size-1)
+                                                                )
+        self.widgets["labels"]["category_label"].place(relx=0.015, rely=0.05, anchor="w")
+        
+        # Add drop-down menu for selecting channel category
+        self.widgets["inputs"]["category_dropdown"] = ctk.CTkComboBox(self.widgets["tabs"].tab("Channels"),
+                                                                      values = self.channel_categories,
+                                                                      command=self.update_channel_category,
+                                                                      font=("Arial", self.btn_font_size-5),
+                                                                      state="readonly",
+                                                                      )
+        self.widgets["inputs"]["category_dropdown"].place(relx=0.125, rely=0.05, anchor="w")
+        
+        
+    def update_channel_category(self, value):
+        """
+        Update the channel tab when the category drop-down menu is changed
+        """
+        print(value)
+        
+        # Create channel selection page for the selected category
+        self.update_channel_category_page(value)
+        
+    
+    def update_channel_category_page(self, category: str):
+        """
+        Update the current channel selection page for the given category
+
+        Args:
+            category (str): ['environment', 'powertrain', 'brakes', 'suspension', 'steering', 'tires', 'vehicle', 'laps']
+        """
+        # Validate category
+        if not category in self.channel_categories:
+            raise ValueError(f"MainScreen.create_channel_category_page(): Invalid channel category '{category}'")
+        
+        # Check if frame for the category already exists
+        if not f"channel_page_{category}" in self.widgets["frames"]:
+            self.create_channel_category_page(category)
+        
+        # Hide the current channel selection page and show the new one
+        if "channel_page" in self.widgets["frames"]:
+            self.widgets["frames"]["channel_page"].place_forget()
+            
+        self.widgets["frames"]["channel_page"] = self.widgets["frames"][f"channel_page_{category}"]
+        self.widgets["frames"]["channel_page"].place(relx=0.5, rely=0.7, anchor="center")
+            
+    
+    def create_channel_category_page(self, category: str):
+        """
+        Create a selection page for the channels in a given category
+        """ 
+        # Validate category
+        if not category in self.channel_categories:
+            raise ValueError(f"MainScreen.create_channel_category_page(): Invalid channel category '{category}'")
+
+        # Create a scrollable frame for the channel selection page
+        if not f"channel_page_{category}" in self.widgets["frames"]:
+            self.widgets["frames"][f"channel_page_{category}"] = ctk.CTkScrollableFrame(self.widgets["tabs"].tab("Channels"),
+                                                                                width=1260, 
+                                                                                height=720,
+                                                                                corner_radius=0
+                                                                                )
+        
+        
+        
+        
+        
+    def create_tooltips(self):
+        """
+        Create UI tooltips
+        """    
+        # Add tab tooltips
+        pass
         
     def toggle_recording(self):
         """
@@ -92,7 +209,7 @@ class MainScreen(ctk.CTkFrame):
             
             # Reconfigure record status label and image
             self.widgets["labels"]["record_status"].configure(text="Not recording")
-            self.widgets["images"]["record_status"].configure(light_image=Image.open("images/circle.png"), dark_image=Image.open("images/circle.png"), size=(40, 40))
+            self.widgets["images"]["record_status"].configure(light_image=Image.open("images/circle.png"), dark_image=Image.open("images/circle.png"), size=(35, 35))
             self.widgets["labels"]["record_status_image"] = ctk.CTkLabel(self, text="", image=self.widgets["images"]["record_status"])
         else:
             # Start recording
