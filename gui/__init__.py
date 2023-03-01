031488cc3f6a11a5bd8ab19afc33408ee76ca0be
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2023-02-28 19:50:14 -0500

    Set plt to use dark mode

--- gui/__init__.py
+++ gui/__init__.py
@@ -3,7 +3,11 @@ GUI module for iRacing Telemetry Logger
 
 Copyright © Kyle Ward 2023    
 """
+import matplotlib.pyplot as plt
 import customtkinter as ctk
+
+# Set plt to use dark mode
+plt.style.use("dark_background")
 
 ctk.set_appearance_mode("dark") # Dark mode
 ctk.set_default_color_theme("dark-blue") # Dark blue theme
