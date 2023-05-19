ef73ccfb1afc4f0d6354c3e923941f5e95e963c2
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2023-05-19 10:02:37 -0400

    Added APP_METADATA

--- gui/__init__.py
+++ gui/__init__.py
@@ -3,6 +3,7 @@ GUI module for iRacing Telemetry Logger
 
 Copyright © Kyle Ward 2023    
 """
+import datetime as dt
 import matplotlib.pyplot as plt
 import customtkinter as ctk
 
@@ -20,3 +21,20 @@ COLORS = {
     "text_blue": "#132fba"
 }
 
+# App metadata
+APP_NAME = "iRacing Telemetry Logger"
+MAJOR_VERSION = 0
+MINOR_VERSION = 0
+PATCH_VERSION = 1
+BUILD_VERSION = 0
+BUILD_TYPE = "b" # a = alpha build, b = beta build, d = development/debug, pre = pre-release build, r = release build
+VERSION = f"v{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}.{BUILD_VERSION}{BUILD_TYPE}"
+
+
+
+APP_METADATA = {
+    "name": APP_NAME,   
+    "version": VERSION,
+    "author": "Kyle Ward",
+    "copyright": f"Copyright © Kyle Ward {dt.date.today().year}",
+}
